28dbbba9c3ca4ef3c550ce44f977a42092e046dc
Marnen Laibow-Koser <marnen@marnen.org>
2009-05-31 23:29:15 -0400

    Start sketching out Rookie's interface.

--- lib/rookie.rb
+++ lib/rookie.rb
@@ -1,2 +1,10 @@
 class Rookie
+  def initialize(string)
+    @string = string
+  end
+  
+  # Returns a parsed representation of the input string.
+  def to_s
+    @string
+  end
 end
--- spec/rookie_spec.rb
+++ spec/rookie_spec.rb
@@ -5,4 +5,18 @@ describe Rookie do
   it 'should be a valid class' do
     Rookie.class.should == Class
   end
+  
+  describe '(constructor)' do
+    it 'should be valid with 1 String argument' do
+      Rookie.should respond_to(:new)
+      lambda{Rookie.new('a string')}.should_not raise_error
+    end
+  end
+  
+  describe 'to_s' do
+    it 'should return plain strings just the way they were' do
+      string = 'This is a string.'
+      Rookie.new(string).to_s.should == string
+    end
+  end
 end
